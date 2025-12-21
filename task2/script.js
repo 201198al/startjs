5e08274c93cee6b783f412b41effc267bf40403b
201198al <201198al@gmail.com>
2025-12-21 23:16:11 +1000

    поправлена задача 2

--- task2/script.js
+++ task2/script.js
@@ -1,5 +1,15 @@
 const consolelog = document.querySelector('#consolelog');
+const alertEl = document.querySelector('#alert');
+const promptEl = document.querySelector('#prompt');
 
 consolelog.addEventListener('click', () => {
   alert('Служит для вывода информации в консоль');
-})
+});
+
+alertEl.addEventListener('click', () => {
+  alert('Выводит сообщение пользователю');
+});
+
+promptEl.addEventListener('click', () => {
+  prompt('Введите текст');
+});
